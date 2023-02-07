75a000a901c018079c97de132680a8f3f6bc4b46
Md Shafiq Ullah <shohagbit0404@gmail.com>
2023-02-06 19:16:56 -0600

    project upload
    Signed-off-by: Md Shafiq Ullah <shohagbit0404@gmail.com>

---
+++ js/contactme.js
@@ -0,0 +1,53 @@
+
+
+console.log("Called");
+
+// axios.get('https://reqres.in/api/users?page=2')
+// .then((res)=> console.log(res))
+// .catch((err)=> console.log(err));
+
+
+
+// $("#contactme-form").on("submit", function (event) {
+//     console.log("Submit button click");
+   
+// });
+
+function objectifyForm(formArray) {
+    //serialize data function
+    var returnArray = {};
+    for (var i = 0; i < formArray.length; i++){
+        returnArray[formArray[i]['name']] = formArray[i]['value'];
+    }
+    return returnArray;
+}
+
+$("#contactme-form").validator().on("submit", function (event) {
+ console.log("Javascript called");
+
+ var formData = $("#contactme-form").serializeArray();
+ var formSubmit = objectifyForm(formData);
+ var simplified = JSON.stringify(formSubmit);
+ console.log(simplified);
+
+ axios.post('https://130kcncfh2.execute-api.us-east-1.amazonaws.com/v1/callstepfunction', 
+{
+    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
+    ,
+    //"input": "{\"sendername\": \"Sujit\",\"emailaddress\": [\"shafiqcs516@dispostable.com\"],\"telephone\": \"phone\",\"sendersubject\": \"subject\",\"sendermessage\":\"comment\"}",
+    "stateMachineArn": "arn:aws:states:us-east-1:717879216935:stateMachine:SMForEmailSend",
+    "input": simplified
+})
+  .then((response) => {
+    console.log("shafiq - OK");
+    //alert("Your Requested Submitted Successfully ");
+    console.log(response);
+  })
+  .catch( (error) => {
+    console.log("shafiq - error");
+    //alert("Ops !!! Failed to Submit. Retry again...");
+    console.log(error);
+  });
+  return false;
+});
+
